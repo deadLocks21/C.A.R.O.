b3f4e82aabef00ba227e1454acdc2f441240f18d
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2019-10-28 22:47:49 +0200

    Gestion de la création de boutons.

--- MonApp/widgetsBase.py
+++ MonApp/widgetsBase.py
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS canvas(
      borderwidth INTEGER DEFAULT 2,
      background VARCHAR(43) DEFAULT '#E4E4E4',
      height INTEGER NOT NULL,
-     highlightbackground VARCHAR(43) DEFAULT 'none',
-     highlightcolor VARCHAR(43) DEFAULT 'none',
+     highlightbackground VARCHAR(43) DEFAULT 'None',
+     highlightcolor VARCHAR(43) DEFAULT 'None',
      highlightthickness INTEGER DEFAULT 1,
      relief VARCHAR(43) DEFAULT 'flat',
      width INTEGER NOT NULL,
@@ -23,6 +23,26 @@ CREATE TABLE IF NOT EXISTS canvas(
 );
 """)
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS button(
+    nomButton VARCHAR(43) NOT NULL,
+    root VARCHAR(43) NOT NULL,
+    text TEXT DEFAULT '',
+    textvariable VARCHAR(43) DEFAULT 'None',
+    relief VARCHAR(43) DEFAULT 'flat',
+    bg VARCHAR(43) DEFAULT '#AAAAAA',
+    fg VARCHAR(43) DEFAULT '#000000',
+    font VARCHAR(43) DEFAULT 'myFont',
+    image VARCHAR(43) DEFAULT 'None',
+    borderwidth INTEGER DEFAULT 2,
+    x INTEGER DEFAULT 0,
+     y INTEGER DEFAULT 0,
+    width INTEGER NOT NULL,
+    height INTEGER NOT NULL,
+    command VARCHAR(43) DEFAULT 'None'
+    );
+""")
 
+conn.commit()
 
 conn.close()
