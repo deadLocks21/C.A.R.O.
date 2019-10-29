fcc5a16505411d21395a5aa05ec8bf3e0caaecba
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2019-10-29 13:14:18 +0200

    Reboot graphique
    Suite à la complexité du développement d'une aide à la création de visuel pour tkinter, j'ai décidé de me simplifier la tâche pour cette fois et de me cpntenter de quelque chose de simple et fonctionnelle.

--- CAA.py
+++ CAA.py
@@ -1,25 +1,3 @@
 #! /usr/bin/env python374
 # -*- encoding: utf-8 *-
 
-from MonApp.MonApp import *
-import sqlite3
-
-APPLICATION_NAME = "CAA-createAnAlgorithm"  # Nom de l'application
-conn = sqlite3.connect('widgets.db')
-cursor = conn.cursor()
-
-
-root = createRoot()  # Creation de la fenetre tkinter
-w = getScreenWidth(root)  # Stockage de la largeur de l'ecran
-h = getScreenHeight(root)  # Stockage de la hauteur de l'ecran
-
-setFullScreen(root)  # Mettre l'application en plein ecran
-setRootName(root, APPLICATION_NAME)  # Renommer le nom de la fenetre
-
-# main = CPcanvas("main", root)  # Création et affichage du canvas main
-
-# bt6 = CPbutton("bt", root)
-lb1 = CPlabel("lb1", root)
-
-
-startApp(root)  # Lancement de l'application
