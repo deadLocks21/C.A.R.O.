12d0fa6a5816c53b2fccb74373df645ec0e160ba
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2019-10-30 22:57:33 +0100

    Graphique presque fini !!

--- t.py
+++ t.py
@@ -1,4 +1,2 @@
-boutonGen_pil = Image.open('images/boutonGen.png')
-boutonGenR = (int(h * POURCENT_BARRE * 0.03), int(h * POURCENT_BARRE))
-boutonGen_img = ImageTk.PhotoImage(boutonGen_pil.resize(boutonGenR))
-fonctionC.create_image(w/2, h/2+h*0.05, image=boutonGen_img)
+resProgrammeEntry = Text(programmeC)
+resProgrammeEntry.place(x=(w*0.026), y=h*0.6389, width=int(w*0.4375), height=int(h*0.1204))
